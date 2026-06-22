bd00afe57e3eb9df1b104f36508f7672ad69e773
plumager <plumager@users.noreply.github.com>
2026-06-23 06:12:46 +0800

    Keep NanoPi R4S in config so its U-Boot is built for DoorNet2

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -6,9 +6,7 @@ define Device/embedfire_doornet2
   UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
-  IMAGES := rootfs.tar.gz
-  IMAGE/rootfs.tar.gz := rootfs | tar.gz
-  IMAGES += kernel.bin
-  IMAGE/kernel.bin := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
+  IMAGES := sysupgrade.img.gz
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += embedfire_doornet2
