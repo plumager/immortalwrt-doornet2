3cb1889495ae1195166824b588a43d3e316b9f84
plumager <plumager@users.noreply.github.com>
2026-06-23 02:37:32 +0800

    Fix: use pine64-img instead of pine64-bin (no proprietary idbloader)

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -6,6 +6,6 @@ define Device/embedfire_doornet2
   UBOOT_DEVICE_NAME := doornet2-rk3399
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | pine64-bin | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += embedfire_doornet2
