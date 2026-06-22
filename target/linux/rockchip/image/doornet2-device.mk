238254fa6706f4a62389d7350d57ac7e0497b330
plumager <plumager@users.noreply.github.com>
2026-06-23 01:23:32 +0800

    Fix: add KERNEL_LOADADDR to device definition

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -2,6 +2,7 @@ define Device/embedfire_doornet2
   DEVICE_VENDOR := EmbedFire
   DEVICE_MODEL := DoorNet2
   DEVICE_DTS := rk3399-embedfire-doornet2
+  KERNEL_LOADADDR := 0x00080000
   UBOOT_DEVICE_NAME := doornet2-rk3399
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
