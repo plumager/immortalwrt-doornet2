3b23880f459cbb05e4be4b7030e4aa19498fa2c7
plumager <plumager@users.noreply.github.com>
2026-06-23 03:54:59 +0800

    Fix: use nanopi-r4s-rk3399 U-Boot (supported by ImmortalWrt)

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -3,7 +3,7 @@ define Device/embedfire_doornet2
   DEVICE_MODEL := DoorNet2
   DEVICE_DTS := rk3399-embedfire-doornet2
   KERNEL_LOADADDR := 0x00080000
-  UBOOT_DEVICE_NAME := doornet2-rk3399
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
   IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | pine64-img | gzip | append-metadata
