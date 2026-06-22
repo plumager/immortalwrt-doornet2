56d07f2aa3034f7156324dd8cb9c611cb9991d95
plumager <plumager@users.noreply.github.com>
2026-06-23 04:53:42 +0800

    Switch to rootfs.tar.gz + kernel.bin (no U-Boot dependency)

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -6,7 +6,9 @@ define Device/embedfire_doornet2
   UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
-  IMAGES := sysupgrade.img.gz
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | gzip | append-metadata
+  IMAGES := rootfs.tar.gz
+  IMAGE/rootfs.tar.gz := rootfs | tar.gz
+  IMAGES += kernel.bin
+  IMAGE/kernel.bin := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
 endef
 TARGET_DEVICES += embedfire_doornet2
