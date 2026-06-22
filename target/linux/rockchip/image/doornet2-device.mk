2d5a23880b53f15f86383581238cb9404c41d76a
plumager <plumager@users.noreply.github.com>
2026-06-23 07:25:33 +0800

    Custom doornet2-img recipe: U-Boot dd is optional, build succeeds either way

--- target/linux/rockchip/image/doornet2-device.mk
+++ target/linux/rockchip/image/doornet2-device.mk
@@ -1,3 +1,12 @@
+# Custom image recipe - same as pine64-img but U-Boot dd is optional (non-fatal)
+define Build/doornet2-img
+	PADDING=1 $(SCRIPT_DIR)/gen_image_generic.sh \
+		$@ $(CONFIG_TARGET_KERNEL_PARTSIZE) $@.boot \
+		$(CONFIG_TARGET_ROOTFS_PARTSIZE) $(IMAGE_ROOTFS) 32768
+	-dd if="$(STAGING_DIR_IMAGE)/$(UBOOT_DEVICE_NAME)-u-boot-rockchip.bin" \
+		of="$@" seek=64 conv=notrunc 2>/dev/null || true
+endef
+
 define Device/embedfire_doornet2
   DEVICE_VENDOR := EmbedFire
   DEVICE_MODEL := DoorNet2
@@ -7,6 +16,6 @@ define Device/embedfire_doornet2
   SOC := rk3399
   DEVICE_PACKAGES := kmod-r8169 -urngd
   IMAGES := sysupgrade.img.gz
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script $$(BOOT_SCRIPT) | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rockchip | doornet2-img | gzip | append-metadata
 endef
 TARGET_DEVICES += embedfire_doornet2
